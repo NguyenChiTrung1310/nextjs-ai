102484ed8f570c1a6f5763d9dd36cab990719a31
Trung Nguyen <nguyenchitrung1310@gmail.com>
2025-05-16 17:23:53 +0700

    Create component Image Upload

---
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageUpload = () => {
+  return (
+    <div className='mt-10'>
+      <div className='grid grid-cols-1 gap-10 md:grid-cols-2' />
+    </div>
+  )
+}
+
+export default ImageUpload
